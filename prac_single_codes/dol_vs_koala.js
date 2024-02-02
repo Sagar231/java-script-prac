fc4190b9da1eafb5bf2fa03abac2500ba4f5cf79
Sagar231 <sagarfeb298@gmail.com>
2024-02-02 15:58:39 +0530

    added a code for practice

---
+++ prac_single_codes/dol_vs_koala.js
@@ -0,0 +1,16 @@
+const calcAverage = (a,b,c) => (a+b+c)/3
+
+const checkWinner = function(scDol,scKol){
+    if(scDol>=2*scKol){
+        return `Dolphins win (${scDol} vs. ${scKol})`;
+    } else if(scKol>=2*scDol){
+        return `Koalas win (${scKol} vs. ${scDol})`;
+    } else{
+        return `No team wins...`;
+    }
+
+}
+let scoreDolphins = calcAverage(44,2,7)
+let scoreKoalas = calcAverage(65,54,49)
+
+console.log(checkWinner(scoreDolphins,scoreKoalas))
